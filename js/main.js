b903b2a04d623480c600cfd29482c47d881b925c
silliecelia <chungsyc@gmail.com>
2016-03-19 14:35:36 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ $( ".hamburger-icon" ).click(function() {
 $( ".cancel-icon" ).click(function() {
   $( "nav" ).slideUp( 1000 );
 });
+
+var $dyk = $('did-you-know-text'),
+    cycle;
+
+(cycle = function() {
+    $('.text-1').delay(2000).fadeOut('slow');
+    $('.text-2').delay(3000).fadeIn('slow');
+    $('.text-2').delay(3500).fadeOut('slow');
+    $('.text-1').delay(5000).fadeIn('slow', cycle);
+/*    $('.text-3').delay(2000).fadeOut('slow');
+    $('.text-1').delay(3000).fadeIn('slow', cycle);*/
+})();
